ba18675a8f901bb32e4dedb9700df6eba07818ac
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2020-07-29 11:37:38 -0400

    there is an attempt (finding your sequence through pi)

---
+++ pi/pi.java
@@ -0,0 +1,19 @@
+public class pi {
+
+	public static void main(String[] args){
+		double pi;
+		int better = 1000;
+		pi = 3*better;
+
+		System.out.println((4*better)/(2*3*4));
+		pi += ((4*better)/(2*3*4));
+		pi -= ((4*better)/(4*5*6));
+
+		pi += ((4*better)/(6*7*8));
+		pi -= ((4*better)/(8*9*10));
+
+		System.out.println(pi);
+		}
+
+
+	}
